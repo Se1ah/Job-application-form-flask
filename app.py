7fdc20a6d4b9a2949208a2063672d82678a130a5
salah <salah.m@outlook.com>
2023-04-07 21:55:35 +0300

    Display success message upon submitting the form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -30,6 +30,7 @@ def index():
         form = Form(first_name=first_name, last_name=last_name, email=email, date=date_object, occupation=occupation)
         db.session.add(form)
         db.session.commit()
+        flash("Your form has submitted successfully", "success")
 
     return render_template("index.html")
 
